5f8877981d84c4060a8f1173f5232579295e98c6
datvu99 <thanhdat.vu@realmemobile.vn>
2022-10-10 22:28:06 +0700

    detail

--- js/javascript.js
+++ js/javascript.js
@@ -39,5 +39,28 @@ buttonPrev.addEventListener("click",function(){
 })
 
 
+//slick-slider detail product
+
+$('.img-main').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.img-detail-slider'
+});
+$('.img-detail-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.img-main',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+$(".color").click(function(){
+  $(".color").prop( "checked", false );
+  $(this).prop( "checked", true );
+});
+
 
 
